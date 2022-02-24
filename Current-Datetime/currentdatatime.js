70836d954306418d24d8cd6f0546cca5f35ed256
Kaide <kaidewu@protonmail.com>
2022-02-24 23:26:38 +0100

    Update current datetime

---
+++ Current-Datetime/currentdatatime.js
@@ -0,0 +1,15 @@
+// Adding 0 before each minutes and seconds
+function addingZero(time){
+    if (time <= 9){
+        return '0' + time;
+    }
+    return time;
+}
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let currentDate = new Date();
+let cToday = 'Today is: ' + currentDate.toLocaleString(
+    // If it's default, set the language of your pc
+    'default', {weekday: 'long'}
+);
+let cHour = 'Current time is: ' + currentDate.getHours() + ':' + addingZero(currentDate.getMinutes()) +':' + addingZero(currentDate.getSeconds());
+console.log(cToday,'\n',cHour);
