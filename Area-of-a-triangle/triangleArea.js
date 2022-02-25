62e6a8b49f53f4aacc8a4275268ec4024c13c314
Kaide <kaidewu@protonmail.com>
2022-02-25 17:31:44 +0100

    Update area of the triangle exercise

--- Area-of-a-triangle/triangleArea.js
+++ Area-of-a-triangle/triangleArea.js
@@ -2,6 +2,9 @@
 const a = 5;
 const b = 6;
 const c = 7;
-
-
+// First we need to find out the semiperimeter
+var s = (a + b + c) / 2;
+// Now we calculate the area of the triangle
+var area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+console.log(area)
 
