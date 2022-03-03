6d960400500c18abf6a01aba342913a3017fdcf0
Kaide <kaidewu@protonmail.com>
2022-03-03 19:23:57 +0100

    Unfinishied Merge Sort Exercise

--- Merge-Sort/mergeSort.js
+++ Merge-Sort/mergeSort.js
@@ -1,37 +1,21 @@
-var numArray = [3, 4, 5, 6, 1, 2, 9, 0, 8, 7];
-
+var ArrayList = [3, 4, 5, 6, 1, 2, 9, 0, 8, 7];
+var counter = 0;
+var Num_Array = [];
 // Error control: if the length array is lower than 1, do nothing
-if (numArray.length <= 1){
+if (ArrayList.length < 2){
     return;
 }
 
-// Take out the half of the length the array
-var half_numArray = Math.floor(numArray.length / 2);
+console.log(ArrayList);
+while (counter < ArrayList.length){
+    counter += 1;
+    console.log();
+    for (let i = 0; i < Math.floor(ArrayList.length / 2); i++){
+        Num_Array.push(ArrayList[i]);
+    }
+    console.log(Num_Array);
+    Num_Array = [];
 
-// First half of the array
-var FirstHalf = [];
-for (let i = 0; i < half_numArray; i++){
-    FirstHalf.push(numArray[i]);
 }
-
-// Second half of the array
-var SecondHalf = [];
-for (let i = half_numArray; i < numArray.length; i++){
-    SecondHalf.push(numArray[i]);
-}
-
-// Take out the half of first half of the array
-var half_FistHalf = Math.floor(FirstHalf.length / 2);
-
-// First half of the half of first of the array
-var First_FirstHalf = []
-for (let i = 0; i < half_FistHalf; i++){
-    First_FirstHalf.push(FirstHalf[i]);
-}
-
-console.log(numArray);
-console.log();
-console.log(FirstHalf, SecondHalf);
-console.log();
-console.log(First_FirstHalf);
+console.log(ArrayList.sort());
 
